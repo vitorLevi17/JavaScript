2e6975e01e0d175742611cf2d67115a92ccb948f
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-02-06 16:16:17 -0300

    Banco e HANDL

--- HTTP/app.js
+++ HTTP/app.js
@@ -1,24 +1,78 @@
 const express = require("express")
 const app = express();
+const hand = require('express-handlebars')
 
-app.get("/",function rota(req,res) {
-    res.sendFile(__dirname + "/templates/index.html")
+
+    //INDETIFICAÇÕES DO BANCO
+const Sequelize = require ('sequelize')
+const sequelize = new Sequelize('cadastro','root','',{
+    host: "localhost",
+    dialect: 'mysql'
 })
 
-app.get("/login",function rota(req,res) {
-    res.sendFile(__dirname + "/templates/login.html")
-})
-
-
-app.get("/perfil",function rota(req,res) {
-    res.send("Bem ")
-})
-
-
-app.get("/ola/:nome/:cargo/:teste",function rota(req,res) {
-    res.send("Olá"+req.params.nome)
-})
+//config
+    //template end
+    app.engine('hand',handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
 
 app.listen(8081, function servidor() {
     console.log("Servidor rodando")
 })
+
+app.get("/",function rota(req,res) {
+    res.sendFile(__dirname + "/templates/index.html")
+})
+/*
+app.get("/login",function rota(req,res) {
+    res.sendFile(__dirname + "/templates/login.html")
+})
+
+app.get("/ola/:nome/:cargo/:teste",function rota(req,res) {
+    res.send("Olá"+req.params.nome)
+})*/
+
+
+
+/*AUTENTICAÇÃO BD
+
+sequelize.authenticate().then(function () {
+    console.log("Tudo certo")
+}).catch(function (erro) {
+    console.log("Falha ao conectar"+erro)
+}) 
+
+//CRIAR TABELA BD
+const Posta = sequelize.define('posta',{
+    titulo : {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Posta.create({
+    titulo: "Unha pintada",
+    conteudo: "Eu vou postar so pra doer em vc"
+})
+
+const Func = sequelize.define('func',{
+    nome : {
+        type: Sequelize.STRING
+    },
+    idade :{
+        type: Sequelize.INTEGER
+    },
+    UF : {
+        type: Sequelize.STRING
+    },
+
+    DESCRICAO :{
+        type: Sequelize.TEXT
+    }
+})
+
+//SICRONIZAR COM BD
+Func.sync({force: true})
+*/
+  
