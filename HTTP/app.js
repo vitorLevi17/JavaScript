7a6b5663123cd2a59c0a8f9d4abb63496b13374e
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-03-07 16:57:07 -0300

    .

--- HTTP/app.js
+++ HTTP/app.js
@@ -4,95 +4,53 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser')
 const Post = require('./models/Posts')
 
-//app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 
-app.engine('handlebars', handlebars.engine ({
-    defaultLayout: 'main', runtimeOptions: {
-        allowProtoPropertiesByDefault: true,
-        allowProtoMethodsByDefault: true
-    }
-}))
+    app.engine('handlebars', handlebars.engine ({
+        defaultLayout: 'main', runtimeOptions: {
+            allowProtoPropertiesByDefault: true,
+            allowProtoMethodsByDefault: true
+        }
+    }))
+    app.set('view engine', 'handlebars');
 
-app.set('view engine', 'handlebars');
 
-//Body parser
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+        //Body parser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
 
-app.listen(8081, function servidor() {
-    console.log("Servidor rodando")
-})
+        app.listen(8081, function servidor() {
+            console.log("Servidor rodando")
+        })
 
-app.get("/NovoPost",function rota(req,res) {
-    res.render('formulario')
-})
+            app.get("/NovoPost",function rota(req,res) {
+                res.render('formulario')
+            })
 
-app.post("/add",function rota(req,res) {
-    Post.create({
-        titulo: req.body.titulo,
-        conteudo: req.body.conteudo
-    }).then(function(){
-        res.redirect('/')
-}).catch(function(erro){
-    res.send("deu ruim aqui"+erro)
-})
-})
+                app.post("/add",function rota(req,res) {
+                    Post.create({
+                        titulo: req.body.titulo,
+                        conteudo: req.body.conteudo
+                    }).then(function(){
+                        res.redirect('/')
+                }).catch(function(erro){
+                    res.send("deu ruim aqui"+erro)
+                })
+                })
 
-app.get('/',function rota(req,res) {
-    Post.findAll({order: [['id','DESC']]}).then(function(posts){
-        console.log(posts)
-        res.render('home',{posts: posts})   
-    })
-})
+                    app.get('/',function rota(req,res) {
+                        Post.findAll({order: [['id','DESC']]}).then(function(posts){
+                            console.log(posts)
+                            res.render('home',{posts: posts})   
+                        })
+                    })
 
-app.get('/deletar/:id',function rota(req,res){
-    Post.destroy({where: {'id':req.params.id}}).then(function(){
-        res.send("mensagem deletada")
-}).catch(function(erro){
-    res.send("mensagem não existe")
-})
-})
+                        app.get('/deletar/:id',function rota(req,res){
+                            Post.destroy({where: {'id':req.params.id}}).then(function(){
+                                res.send("mensagem deletada")
+                        }).catch(function(erro){
+                            res.send("mensagem não existe")
+                        })
+                        })
 
-/*AUTENTICAÇÃO BD
 
-sequelize.authenticate().then(function () {
-    console.log("Tudo certo")
-}).catch(function (erro) {
-    console.log("Falha ao conectar"+erro)
-}) 
-
-//CRIAR TABELA BD
-const Posta = sequelize.define('posta',{
-    titulo : {
-        type: Sequelize.STRING
-    },
-    conteudo: {
-        type: Sequelize.TEXT
-    }
-})
-
-Posta.create({
-    titulo: "Unha pintada",
-    conteudo: "Eu vou postar so pra doer em vc"
-})
-
-const Func = sequelize.define('func',{
-    nome : {
-        type: Sequelize.STRING
-    },
-    idade :{
-        type: Sequelize.INTEGER
-    },
-    UF : {
-        type: Sequelize.STRING
-    },
-
-    DESCRICAO :{
-        type: Sequelize.TEXT
-    }
-})
-
-//SICRONIZAR COM BD
-Func.sync({force: true})
-*/
   
--- HTTP/models/db.js
+++ HTTP/models/db.js
@@ -10,3 +10,47 @@ module.exports = {
     Sequelize: Sequelize,
     sequelize:sequelize
 }
+
+
+/*AUTENTICAÇÃO BD
+
+sequelize.authenticate().then(function () {
+    console.log("Tudo certo")
+}).catch(function (erro) {
+    console.log("Falha ao conectar"+erro)
+}) 
+
+//CRIAR TABELA BD
+const Posta = sequelize.define('posta',{
+    titulo : {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Posta.create({
+    titulo: "Unha pintada",
+    conteudo: "Eu vou postar so pra doer em vc"
+})
+
+const Func = sequelize.define('func',{
+    nome : {
+        type: Sequelize.STRING
+    },
+    idade :{
+        type: Sequelize.INTEGER
+    },
+    UF : {
+        type: Sequelize.STRING
+    },
+
+    DESCRICAO :{
+        type: Sequelize.TEXT
+    }
+})
+
+//SICRONIZAR COM BD
+Func.sync({force: true})
+*/
